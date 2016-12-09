3827130d003acf7603ddcce49a8e2604d7cae708
Antonio Villagra De La Cruz <antonio.villagra.de.la.cruz@gmail.com>
2016-12-09 21:43:32 +0100

    Add basic implementation of 'DropMessage' scene

--- scenes/ios/DropMessage.js
+++ scenes/ios/DropMessage.js
@@ -2,16 +2,43 @@
 
 import React from "react"
 import {
+    Button,
+    Image,
+    KeyboardAvoidingView,
     Text,
-    View
+    TextInput
 } from "react-native"
 
 export default class DropMessage extends React.Component {
     render() {
         return (
-            <View>
-                <Text>Drop a Message</Text>
-            </View>
+            <KeyboardAvoidingView
+                behaviour={'padding'}
+                style={styles.container}>
+                <Image 
+                    source={""}
+                    style={styles.image} />
+                <Text
+                    style={styles.title}>
+                    Drop a message!
+                </Text>
+                <TextInput
+                    autoFocus={true}
+                    maxLength={144}
+                    multiline={false}
+                    placeholder={"Whoop whoop!"}
+                    returnKeyType={"go"}
+                    style={styles.input}
+                    onChangeText={()=>{}}
+                    onSubmitEditing={()=>{}}
+                />
+                <Button
+                    disabled
+                    accessibilityLabel={"Post a message!"}
+                    title={"Post!"}
+                    onPress={()=>{}}
+                />
+            </KeyboardAvoidingView>
         )
     }
 }
@@ -20,5 +47,21 @@ export default class DropMessage extends React.Component {
 import { StyleSheet } from "react-native"
 
 const styles = StyleSheet.create({
-
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    title: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    input: {
+        height: 40, 
+        borderColor: 'gray', 
+        borderWidth: 1,
+        padding: 5,
+        marginBottom: 5,
+    },
 })
