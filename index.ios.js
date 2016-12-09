4283ac14ce1c832f512b49e58b93ba172653d4d7
Antonio Villagra De La Cruz <antonio.villagra.de.la.cruz@gmail.com>
2016-12-09 20:31:35 +0100

    Add basic navigation and navigation bar

--- index.ios.js
+++ index.ios.js
@@ -2,17 +2,82 @@
 
 import React from "react"
 import {
-    AppRegistry
+    AppRegistry,
+    Navigator,
+    Text,
+    View
 } from "react-native"
 
 import {
+    DropMessage,
+    MessageDetails,
+    MessageList,
+    Settings,
     Welcome
 } from "./scenes/ios"
 
 export default class Kruzit extends React.Component {
     render() {
         return (
-            <Welcome />
+            <Navigator
+                initialRoute={{ id: "welcome" }}
+                renderScene={ this.renderScene }
+                navigationBar={ this.navigationBar() }
+            />
+        )
+    }
+
+    renderScene(route, navigator) {
+        switch (route.id) {
+            case "drop":
+                return (<DropMessage />)
+            case "list":
+                return (<MessageList />)
+            case "message":
+                return (<MessageDetails />)
+            case "settings":
+                return (<Settings />)
+            case "welcome":
+                return (<Welcome />)
+        }
+    }
+
+    navigationBar() { 
+        return (
+            <Navigator.NavigationBar 
+                routeMapper={{
+                    LeftButton: (route, navigator) => {
+                        return (
+                            <Text 
+                                onPress={() => {
+                                    navigator.replace({ id:"settings" })
+                                }}>
+                                Settings
+                            </Text>
+                        )
+                    },
+                    Title: (route, navigator) => {
+                        return (
+                            <Text 
+                                onPress={() => {
+                                    navigator.replace({ id:"drop" })
+                                }}>
+                                Drop
+                            </Text>
+                        )
+                    },
+                    RightButton: (route, navigator) => {
+                        return (
+                            <Text 
+                                onPress={() => {
+                                    navigator.replace({ id:"list" })
+                                }}>
+                                List
+                            </Text>
+                        )
+                    }
+                }}
+            />
         )
     }
 }
