c2fe7d794f1e3eb59ba77c4aeeeddfcb3bb02e5a
Antonio Villagra De La Cruz <antonio.villagra.de.la.cruz@gmail.com>
2016-12-09 19:07:47 +0100

    Move scenes to self-contained folder

--- index.ios.js
+++ index.ios.js
@@ -2,28 +2,17 @@
 
 import React from "react"
 import {
-    AppRegistry,
-    Text,
-    View
+    AppRegistry
 } from "react-native"
 
-import styles from "./styles"
+import {
+    Welcome
+} from "./scenes/ios"
 
 export default class Kruzit extends React.Component {
     render() {
         return (
-            <View style={styles.container}>
-                <Text style={styles.welcome}>
-                    Welcome to React Native!
-                </Text>
-                <Text style={styles.instructions}>
-                    To get started, edit index.ios.js
-                </Text>
-                <Text style={styles.instructions}>
-                    Press Cmd+R to reload,{"\n"}
-                    Cmd+D or shake for dev menu
-                </Text>
-            </View>
+            <Welcome />
         )
     }
 }
---
+++ scenes/ios/index.js
@@ -0,0 +1,3 @@
+// @flow
+
+export { default as Welcome } from "./Welcome"
