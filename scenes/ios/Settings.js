128a894681336f2f825cce010a35cc84a8d6698f
Antonio Villagra De La Cruz <antonio.villagra.de.la.cruz@gmail.com>
2016-12-10 15:28:01 +0100

    Add basic implementation of 'Settings' scene

--- scenes/ios/Settings.js
+++ scenes/ios/Settings.js
@@ -2,6 +2,9 @@
 
 import React from "react"
 import {
+    Button,
+    Slider,
+    Switch,
     Text,
     View
 } from "react-native"
@@ -9,8 +12,71 @@ import {
 export default class Settings extends React.Component {
     render() {
         return (
-            <View>
-                <Text>Settings</Text>
+            <View
+                style={styles.container}>
+                <View>
+                    <View>
+                        <Text>
+                            Notifications
+                        </Text>
+                        <Switch
+                            value={true}
+                            onValueChange={()=>{}}
+                        />
+                    </View>
+                    <View>
+                        <Text>
+                            Allow push notification of messages at the chosen distance.
+                        </Text>
+                        <View>
+                            <Text>
+                                Distance
+                            </Text>
+                            <View>
+                                <Slider
+                                    maximumValue={100}
+                                    minimumValue={1}
+                                    step={1}
+                                    value={10}
+                                    onSlidingComplete={()=>{}}
+                                    onValueChange={()=>{}}
+                                />
+                                <Text>km</Text>
+                            </View>
+                        </View>
+                    </View>
+                </View>
+                <View>
+                    <View>
+                        <Text>
+                            History
+                        </Text>
+                        <Switch
+                            value={true}
+                            onValueChange={()=>{}}
+                        />
+                    </View>
+                    <View>
+                        <Text>
+                            Save all the message you come accross!
+                        </Text>
+                        <View>
+                            <Button
+                                accessibilityLabel={"Clear all history"}
+                                title={"Clear history"}
+                                onPress={()=>{}}
+                            />
+                        </View>
+                    </View>
+                </View>
+                <Button
+                    accessibilityLabel={"Delete account"}
+                    title={"Delete account"}
+                    onPress={()=>{}}
+                />
+                <Text>
+                    Copyright bla bla bla ...
+                </Text>
             </View>
         )
     }
@@ -20,5 +86,9 @@ export default class Settings extends React.Component {
 import { StyleSheet } from "react-native"
 
 const styles = StyleSheet.create({
-
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
 })
