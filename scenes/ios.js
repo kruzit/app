afb222fc145e93abd58523ea9f3f2d9cf0d6a4c3
Antonio Villagra De La Cruz <antonio.villagra.de.la.cruz@gmail.com>
2016-12-10 15:42:45 +0100

    Re-order scenes

---
+++ scenes/ios.js
@@ -0,0 +1,7 @@
+// @flow
+
+export { default as DropMessage } from "./common/DropMessage"
+export { default as MessageDetails } from "./common/MessageDetails"
+export { default as MessageHistory } from "./common/MessageHistory"
+export { default as Settings } from "./common/Settings"
+export { default as Welcome } from "./ios/Welcome"
--- scenes/ios/index.js
+++
@@ -1,7 +0,0 @@
-// @flow
-
-export { default as DropMessage } from "./DropMessage"
-export { default as MessageDetails } from "./MessageDetails"
-export { default as MessageHistory } from "./MessageHistory"
-export { default as Settings } from "./Settings"
-export { default as Welcome } from "./Welcome"
